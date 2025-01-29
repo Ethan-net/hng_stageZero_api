9b4935f824757f36fd9a391932e9863e4416ed5f
Akorede Ethan <koredeajibola@gmail.com>
2025-01-29 11:39:40 +0100

    updated

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+
+const router = require("./routes/route");
+
 require("dotenv").config();
+
+const port = process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors({ credentials: true }));
+
+app.use("/get", router);
+
+app.listen(port, () => {
+  console.log(`now connected on port ${port}`);
+});
---
+++ routes/route.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+router = express.Router();
+
+const dateTime = new Date().toISOString();
+
+router.get("/mydata", (req, res) => {
+  res.status(200).json({
+    email: "koredeajibola@gmail.com",
+    current_datetime: `${dateTime}`,
+    github_url: "https://github.com/Ethan-net/hng_stageZero_api.git",
+  });
+});
+
+module.exports = router;
