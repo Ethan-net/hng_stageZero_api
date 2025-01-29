cfc8716793b2d4ce67928244c945ee6d65255391
Akorede Ethan <koredeajibola@gmail.com>
2025-01-29 18:28:23 +0100

    updated cors

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
-app.use(cors({ credentials: true }));
+
+app.use(cors({ origin: "*", credentials: true }));
 
 app.use("/get", router);
 
