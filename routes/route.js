64626098eb2ec31bb1cf6ab03b922a7576992ece
Akorede Ethan <koredeajibola@gmail.com>
2025-01-30 11:47:15 +0100

    update

--- routes/route.js
+++ routes/route.js
@@ -8,7 +8,7 @@ router.get("/mydata", (req, res) => {
   res.status(200).json({
     email: "koredeajibola@gmail.com",
     current_datetime: `${dateTime}`,
-    github_url: "https://github.com/Ethan-net/hng_stageZero_api.git",
+    github_url: "https://github.com/Ethan-net/hng_stageZero_api",
   });
 });
 
